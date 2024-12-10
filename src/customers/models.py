4dac166f8a095ac43a72cf350c7e378aa37a9522
Carlos-860 <carlostimotheus5@gmail.com>
2024-12-10 22:28:09 +0200

    DR-1 Add field constraints and validation & Model docstring to Customer model

--- src/customers/models.py
+++ src/customers/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    name = models.CharField(max_length=120)
+    """
+    Represents a customer in the system.
+    Stores basic customer information including business name and logo.
+    """
+    name = models.CharField(max_length=120, blank=False, help_text='Customer\'s business name')
     logo = models.ImageField(upload_to='customers', default='no_picture.png')
 
     def __str__(self):

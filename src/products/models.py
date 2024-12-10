a954454387472fd47ca7f024b911cdc4ab77c88d
Carlos-860 <carlostimotheus5@gmail.com>
2024-12-10 22:57:32 +0200

    DR-2 Use DecimalField for price to avoid floating-point precision issues

--- src/products/models.py
+++ src/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator
 
 # Create your models here.
 class Product(models.Model):
@@ -8,7 +9,12 @@ class Product(models.Model):
     """
     name = models.CharField(max_length=120, blank=False, help_text='Product\'s name')
     image = models.ImageField(upload_to='products', default='no_picture.png')
-    price = models.FloatField(help_text='in US dollars $')
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        help_text='in US dollars $',
+        validators=[MinValueValidator(0)]
+    )
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 

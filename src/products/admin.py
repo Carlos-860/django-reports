d4fc821fce1f2cfc908d391d08397594beee8875
Carlos-860 <carlostimotheus5@gmail.com>
2024-12-10 23:12:51 +0200

    DR-2 Customizing the admin interface for Product model

--- src/products/admin.py
+++ src/products/admin.py
@@ -3,4 +3,11 @@ from .models import Product
 
 # Register your models here.
 
-admin.site.register(Product)
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'created_at', 'updated_at']
+    list_filter = ['created_at', 'updated_at']
+    search_fields = ['name']
+    readonly_fields = ['created_at', 'updated_at']
+    list_per_page = 2
+    sortable_by = ['price']
